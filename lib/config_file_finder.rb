0427ce73106ad6735e06f89e927fab16ef0b95de
Wayne Allan <wayne.allan@imaginatelabs.com>
2016-07-03 13:09:24 +1000

    Initial commit of bundle gem project files

---
+++ lib/config_file_finder.rb
@@ -0,0 +1,9 @@
+require 'config_file_finder/version'
+
+class ConfigFileFinder
+  def patterns(patterns = {})
+  end
+
+  def find
+  end
+end
---
+++ lib/config_file_finder/version.rb
@@ -0,0 +1,3 @@
+module ConfigFileFinderMeta
+  VERSION = :'0.1.0'
+end
